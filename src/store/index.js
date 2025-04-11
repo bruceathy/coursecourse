9fd9227fd9c30daeba99df7a4423dba51d37b970
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-04-11 18:27:04 -0400

    configure store

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,19 @@
-import { ReactReduxContext } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = configureStore({
+  reducer: counterReducer,
+});
